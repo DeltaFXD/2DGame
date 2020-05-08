b05b292ba7684ef673c6781bc1bbc5694e824b90
DeltaFXD <32136483+DeltaFXD@users.noreply.github.com>
2020-05-08 22:37:41 +0200

    Init Networking Code

---
+++ src/GameEngine/Networking/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Networking
+{
+    class Server
+    {
+
+        public Server(string port)
+        {
+
+        }
+
+        public void Send()
+        {
+
+        }
+
+        public void GetNextReceived()
+        {
+
+        }
+    }
+}
